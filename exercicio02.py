557609f7a0e73b6693bc8418e70d049721bb2450
via <liviarezende04812@gmail.com>
2025-09-28 15:32:05 -0300

    exercicio2

---
+++ exercicio02.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def bin_to_dec(b: str) -> int:
+    decimal : int = 0
+    negativo : bool = False
+
+    if ("-" in b) and (0 != b.index("-")):
+        raise ValueError
+
+    if "-" in b:
+        negativo = True
+        b = b.replace("-", "")
+
+    b = b[::-1]
+    
+    for N in range(len(b)):
+        decimal += int(b[N]) * (2 ** N)
+    
+    if negativo:
+        decimal *= (-1)
+
+    return decimal
+
+def main () -> None:
+    num = input(" > ")
+    
+    print(bin_to_dec(num))
+
+    return None
+
+if __name__ == "__main__":
+    main()
